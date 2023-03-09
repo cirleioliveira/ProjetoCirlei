4e359d0e1766ad988aa414b673d3a82fc217ff8c
cirleioliveira <shirley_1979_5@hotmail.com>
2023-03-08 22:43:31 -0400

    alteração_login

--- assets/css/Css.js
+++ assets/css/Css.js
@@ -24,6 +24,39 @@ const css = StyleSheet.create({
       button__home: {
         marginRight: 40
       },
+      darkbg:{
+        backgroundColor:"#333"
+    },
+    login__logomarca:{
+        marginBottom: 10
+    },
+    login__msg:(text='none')=>({
+        fontWeight:"bold",
+        fontSize: 22,
+        color:"red",
+        marginBottom: 15,
+        display: text
+    }),
+    login__form:{
+        width: "80%"
+    },
+    login__input:{
+        backgroundColor: "#fff",
+        fontSize: 19,
+        padding: 7,
+        marginBottom: 15
+    },
+    login__button:{
+        padding: 12,
+        backgroundColor: "#F58634",
+        alignSelf:"center",
+        borderRadius:5
+    },
+    login__buttonText:{
+        fontWeight:"bold",
+        fontSize: 22,
+        color:"#333"
+    }
   });
 
   export {css};
--- views/Login.js
+++ views/Login.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { KeyboardAvoidingView, TextInput, TouchableOpacity, Image, Text, View, Platform } from 'react-native';
 import { css } from '../assets/css/Css';
 
 export default function Login()
 {
+    const [display, setDisplay]=useState('none');
+
     return(
         <KeyboardAvoidingView behavior = {Platform.OS == "ios" ? "padding" : "height"} style={[css.container, css.darkbg]}>
             <View>
                 <Image source={require('../assets/img/logomarca.png')}/>
             </View>
+            
+            <View>
+                <Text style={css.login__msg(display)}>Usuário ou senha inválidos!</Text>
+            </View>
 
             <View style={css.login__form}>
-                <TextInput placeholder='Usuario:' />
-                <TextInput placeholder='Senha:' secureTextEntry={true} />
-                <TouchableOpacity>
-                    <Text>Entrar</Text>
+                <TextInput style={css.login__input} placeholder='Usuario:' />
+                <TextInput style={css.login__input} placeholder='Senha:' secureTextEntry={true} />
+                <TouchableOpacity style={css.login__button} onPress={() =>setDisplay('flex')}>
+                    <Text style={css.login__buttonText}>Entrar</Text>
                 </TouchableOpacity>
             </View>
         </KeyboardAvoidingView>
