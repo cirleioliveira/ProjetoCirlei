87032daaf857353a65ed16e2b76d357f4856521d
cirleioliveira <shirley_1979_5@hotmail.com>
2023-03-08 22:08:21 -0400

    *Login

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default function App() {
             headerTitleStyle:{fontWeight: 'bold', alignSelf: 'center'}
           }} 
         />
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Login" options={{headerShown:false}} component={Login} />
         <Stack.Screen name="Rastreio" component={Rastreio} />
         {/*<Stack.Screen name="AreaResrita" component={AreaResrita} />*/}
       </Stack.Navigator>
--- views/Login.js
+++ views/Login.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { KeyboardAvoidingView, TextInput, TouchableOpacity, Image, Text, View, Platform } from 'react-native';
+import { css } from '../assets/css/Css';
 
 export default function Login()
 {
     return(
-       
-        <View>
-            <Text>Esse é o componente Login do sistema</Text>
-        </View>
+        <KeyboardAvoidingView behavior = {Platform.OS == "ios" ? "padding" : "height"} style={[css.container, css.darkbg]}>
+            <View>
+                <Image source={require('../assets/img/logomarca.png')}/>
+            </View>
+
+            <View style={css.login__form}>
+                <TextInput placeholder='Usuario:' />
+                <TextInput placeholder='Senha:' secureTextEntry={true} />
+                <TouchableOpacity>
+                    <Text>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     );
 }
 
-//https://www.youtube.com/watch?v=WATNjb3jozI
+//
